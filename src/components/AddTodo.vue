ee363dd33c0273d59c9b1e8769959a8df73f3de9
Condition for project GDSC <ducan.co.6789@gmai.com>
2022-01-28 13:53:12 +0700

    Change

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -74,30 +74,36 @@ input {
 .label {
   width: 100%;
 }
+.label input{
+  border-radius: 5px;
+}
  button {
         display: inline-block;
         width: 80px;
         height: fit-content;
-        margin: 0 5px;
-        padding: 7px;
+        margin-left: 5px;
+        padding: 5px;
         outline: none;
         border: none;
-        border-radius: 2px;
-        font-size: 20px;
+        border-radius: 5px;
+        font-size: 15px;
     }
+.cancel{
+  margin-left: 300px;
+}
     button:hover {
         cursor: pointer;
     }
     button.cancel {
-        background-color: rgb(175, 175, 175);
+        background-color: rgb(155, 148, 148);
     }
     button.cancel:hover {
-        background-color: rgb(145, 145, 145);
+        background-color: rgb(229, 226, 226);
     }
     button.submit {
-        background-color: rgb(166, 166, 240);
+        background-color: rgb(155, 148, 148);
     }
     button.submit:hover {
-        background-color: rgb(130, 130, 236);
+        background-color: rgb(229, 226, 226);
     }
 </style>
